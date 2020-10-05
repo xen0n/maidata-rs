d78271c9c07ea8f6725ef177aa21c15a7684f5fb
WANG Xuerui <git@xen0n.name>
2020-10-05 15:27:50 +0800

    feat: remove some whitespaces

--- src/main.rs
+++ src/main.rs
@@ -48,6 +48,12 @@ fn lex_maidata_inner(s: Span) -> IResult<Span, Maidata> {
 fn lex_keyval(s: Span) -> IResult<Span, KeyVal> {
     use nom::bytes::complete::take_till;
     use nom::character::complete::char;
+    use nom::character::complete::multispace0;
+    use nom::Slice;
+
+    // we might have whitespaces before the first key-value pair, eat them
+    // later pairs have the preceding whitespaces eaten during consumption of the value
+    let (s, _) = multispace0(s)?;
 
     let (s, _) = char('&')(s)?;
     let (s, key_pos) = position(s)?;
@@ -55,6 +61,10 @@ fn lex_keyval(s: Span) -> IResult<Span, KeyVal> {
     let (s, _) = char('=')(s)?;
     let (s, val_pos) = position(s)?;
     let (s, val) = take_till(|x| x == '&')(s)?;
+
+    // strip off trailing newlines from value
+    let num_bytes_to_remove = num_rightmost_whitespaces(val.fragment());
+    let val = val.slice(0..val.fragment().len() - num_bytes_to_remove);
 
     Ok((
         s,
@@ -65,4 +75,44 @@ fn lex_keyval(s: Span) -> IResult<Span, KeyVal> {
             val,
         },
     ))
+}
+
+fn num_rightmost_whitespaces<S: AsRef<str>>(x: S) -> usize {
+    let mut result = 0;
+
+    // only work with bytes for now, simplifies things quite a bit
+    let x = x.as_ref().as_bytes();
+    if x.len() == 0 {
+        return 0;
+    }
+
+    for i in 0..x.len() {
+        let i = x.len() - 1 - i;
+        match x[i] {
+            // '\t' | '\n' | '\r' | ' '
+            0x09 | 0x0a | 0x0d | 0x20 => {
+                result += 1;
+                continue;
+            }
+            // first non-whitespace char backwards
+            _ => break,
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_num_rightmost_whitespaces() {
+        use super::num_rightmost_whitespaces;
+
+        assert_eq!(num_rightmost_whitespaces(""), 0);
+        assert_eq!(num_rightmost_whitespaces("foo"), 0);
+        assert_eq!(num_rightmost_whitespaces("\r\n\r\n"), 4);
+        assert_eq!(num_rightmost_whitespaces("foo\r\n\r\n"), 4);
+        assert_eq!(num_rightmost_whitespaces("foo\r\n\r\nbar"), 0);
+        assert_eq!(num_rightmost_whitespaces("\n\n\nfoo\n\nbar\n"), 1);
+    }
 }
